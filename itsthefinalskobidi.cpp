cb1fa41844d32921e508a7783a7cdc63088c121e
UlissesGoncalves <ulissesgilgoncalves@gmail.com>
2024-11-19 22:05:03 +0000

    The final countdown

---
+++ itsthefinalskobidi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	int n1, i;
+	cout << "da um numero ja";
+	cin >> n1;
+
+	for (i = n1; i >= 0; i--)
+	{
+		cout << i << " \n";
+	}
+	
+}
